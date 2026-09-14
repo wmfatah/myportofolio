fd41601f74cee87a3afcf2146880f2ad2873a4ee
wmfatah <wmfatah@gmail.com>
2026-09-14 22:09:02 +0700

    Complete Tutorial 02 MVT implementation

---
+++ main/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Muhammad Fatahillah Widodo",
+        "npm": "2506623925",
+        "study_program": "S1 Ilmu Komputer",
+        "bio": (
+            "Mahasiswa Ilmu Komputer Universitas Indonesia yang tertarik "
+            "pada pengembangan perangkat lunak dan teknologi."
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Muhammad Fatahillah Widodo",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
